6185f3b5ba65402d1d1c75656898636e5b12c390
ivan <menja08@yahoo.com>
2018-12-18 23:11:12 +0100

    includes all, after ignore was created

---
+++ js/ex3_textBox.js
@@ -0,0 +1,46 @@
+//
+textBox();
+function textBox() {
+    //select box
+    let box = document.getElementById("textBox");
+    //box.style.borderColor = "red";
+    let boxChildren = box.children;
+    let slideLength = boxChildren.length;
+
+    //select dots
+    let threeDots = document.getElementById("threeDots");
+    //threeDots.style.borderColor = "yellow";
+    threeDotsChildren = threeDots.children;
+
+    for(let i = 0; i < slideLength; i++) {
+        boxChildren[i].style.display = "none";
+    }
+
+    let slideCounter = 0;
+    let timeOut;
+    let mouseOverTimeOut;
+    let text;
+
+    setInterval(function () {
+        setTimeout(function () {
+            // display the current image of the slide
+            boxChildren[slideCounter].style.display = "block";
+            threeDotsChildren[slideCounter].style.color = "green";
+
+            //hide the rest of the images
+            let j = 0;
+            for(let i=0; i < slideLength; i++) {
+                if(j !== slideCounter) {
+                    boxChildren[j].style.display = "none";
+                    threeDotsChildren[j].style.color = "grey";
+                }
+                j++;
+            }
+        }, 100);
+        slideCounter++; //next slide to be displayed
+        //reset slideCounter
+        if(slideCounter === slideLength) {
+            slideCounter = 0;
+        }
+    }, 4000);
+}
